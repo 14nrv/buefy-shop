086118f7fcefcb45f5ff3ee019af7f149ccb960a
nRV <contact@styro.fr>
2017-12-21 23:30:04 +0100

    Init nuxt pwa

---
+++ nuxt.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  /*
+  ** Build configuration
+  */
+  build: {},
+  /*
+  ** Headers
+  ** Common headers are already provided by @nuxtjs/pwa preset
+  */
+  head: {},
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Customize app manifest
+  */
+  manifest: {
+    theme_color: '#3B8070'
+  },
+  /*
+  ** Modules
+  */
+  modules: [
+    '@nuxtjs/pwa'
+  ]
+}
---
+++ pages/index.vue
@@ -0,0 +1,101 @@
+<template>
+  <section class="container">
+    <div>
+      <logo/>
+      <h1 class="title">
+        NUXT
+      </h1>
+      <h2 class="subtitle">
+        PWA Vue.js Application
+      </h2>
+      <div :class="['network',online ? 'online' : 'offline']">
+        <div class="circle"></div>
+        {{ online ? 'online' : 'offline' }}
+      </div>
+      <div class="links">
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green" rel="noopener">Documentation</a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey" rel="noopener">GitHub</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+  import Logo from '~/components/Logo.vue'
+
+  export default {
+    components: {Logo},
+    data () {
+      return {
+        online: true
+      }
+    },
+    mounted () {
+      if (!window.navigator) {
+        this.online = false
+        return
+      }
+      this.online = Boolean(window.navigator.onLine)
+      window.addEventListener('offline', this._toggleNetworkStatus)
+      window.addEventListener('online', this._toggleNetworkStatus)
+    },
+    methods: {
+      _toggleNetworkStatus ({ type }) {
+        this.online = type === 'online'
+      }
+    },
+    destroyed () {
+      window.removeEventListener('offline', this._toggleNetworkStatus)
+      window.removeEventListener('online', this._toggleNetworkStatus)
+    }
+  }
+</script>
+
+<style>
+  .container {
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+
+  .title {
+    font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+    display: block;
+    font-weight: 300;
+    font-size: 100px;
+    color: #35495e;
+    letter-spacing: 1px;
+  }
+
+  .subtitle {
+    font-weight: 300;
+    font-size: 42px;
+    color: #526488;
+    word-spacing: 5px;
+    padding-bottom: 15px;
+  }
+
+  .links {
+    padding-top: 15px;
+  }
+
+  .network {
+    font-weight: 400;
+    font-size: 1rem;
+  }
+
+  .network .circle {
+    display: inline-block;
+    width: 1rem;
+    height: 1rem;
+    background: green;
+    padding: .1rem .5rem;
+    border-radius: 1rem;
+  }
+
+  .network.offline .circle {
+    background: red;
+  }
+</style>
