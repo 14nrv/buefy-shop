128fa419d76c38749b6fe64231ef165cfc66610d
nRV <contact@styro.fr>
2017-12-24 04:09:55 +0100

    Added font awesome

--- plugins/buefy.js
+++ plugins/buefy.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import Buefy from 'buefy'
 import 'buefy/lib/buefy.css'
+import 'font-awesome/css/font-awesome.min.css'
 
-Vue.use(Buefy)
+Vue.use(Buefy, {
+  defaultIconPack: 'fa'
+})
