ef2856807ebcf1828ad96f2a47d33da94c6a6b27
nRV <contact@styro.fr>
2018-02-05 20:52:22 +0100

    Improved firebase init

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -2,9 +2,7 @@ import * as firebase from 'firebase/app'
 import 'firebase/database'
 import config from '@/firebase-setup/config'
 
-const firebaseApp = !firebase.apps.length
-  ? firebase.initializeApp(config)
-  : firebase.app()
+const firebaseApp = !firebase.apps.length && firebase.initializeApp(config)
 
 const db = firebaseApp.database()
 
