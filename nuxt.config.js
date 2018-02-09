95ec6b219e7def4c94758a5ce72e1df326c91fe0
nRV <contact@styro.fr>
2018-02-09 21:12:44 +0100

    Fixed FB database connection

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,5 +44,13 @@ module.exports = {
   plugins: [
     '~plugins/buefy',
     '~plugins/firebase'
-  ]
+  ],
+  env: {
+    FB_DATABASE_URL: process.env.FB_DATABASE_URL,
+    FB_PROJECT_ID: process.env.FB_PROJECT_ID,
+    FB_API_KEY: process.env.FB_API_KEY,
+    FB_AUTH_DOMAIN: process.env.FB_AUTH_DOMAIN,
+    FB_STORAGE_BUCKET: process.env.FB_STORAGE_BUCKET,
+    FB_MESSAGE_SENDER_ID: process.env.FB_MESSAGE_SENDER_ID
+  }
 }
