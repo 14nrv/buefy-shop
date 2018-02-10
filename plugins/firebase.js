5bf828627d6e429de67a43bbbb32e01685a439bf
nRV <contact@styro.fr>
2018-02-10 15:16:16 +0100

    Improved firebase init for HMR

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -2,7 +2,9 @@ import * as firebase from 'firebase/app'
 import 'firebase/database'
 import config from '@/firebase-setup/config'
 
-const firebaseApp = !firebase.apps.length && firebase.initializeApp(config)
+const firebaseApp = !firebase.apps.length
+  ? firebase.initializeApp(config)
+  : firebase.app()
 
 const db = firebaseApp.database()
 
