5357c4850889b3b8f6ff46d1d4ba714100a83d1c
nRV <contact@styro.fr>
2018-06-09 20:40:43 +0200

    refactor(store): product mutations

--- store/modules/product/mutations.js
+++ store/modules/product/mutations.js
@@ -1,7 +1,5 @@
-/* eslint-disable no-return-assign */
-
 export default {
-  SWITCH_SALE: state => state.sale = !state.sale,
-  UPDATE_HIGH_PRICE: (state, value) => state.highprice = value,
-  SET_CATEGORY: (state, value) => state.categorySelected = value
+  SWITCH_SALE: state => { state.sale = !state.sale },
+  UPDATE_HIGH_PRICE: (state, value) => { state.highprice = value },
+  SET_CATEGORY: (state, value) => { state.categorySelected = value }
 }
