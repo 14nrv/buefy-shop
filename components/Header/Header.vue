8787418f2f4bb6960b4910cee285dc249ef620e7
nRV <contact@styro.fr>
2018-06-09 20:32:04 +0200

    fix(Header): rendering on small devices

--- components/Header/Header.vue
+++ components/Header/Header.vue
@@ -1,27 +1,30 @@
 <template lang="pug">
-  header.navbar.is-fixed-top.is-light(role="navigation"
-                aria-label="main navigation")
+  header.navbar.is-light(:class="{ 'is-fixed-top': isIndexRoute }",
+                        role="navigation",
+                        aria-label="main navigation")
     .container.is-flex-touch
       .navbar-brand
         nuxt-link.navbar-item(exact, :to="{name: 'index'}")
           strong
             i PlusGrosLeLogo
-      .navbar-end
+      .navbar-end.is-flex-touch
         .navbar-item
-          .field.is-grouped
+          .field
             p.control
-              a.button.is-light(target="_blank",
+              a.button.is-light.is-marginless-mobile(target="_blank",
                                 href="https://github.com/14nrv/buefy-shop",
                                 rel="noopener")
                 span.icon
                   i.fab.fa-github
-                span Fork
+                span.is-hidden-mobile Fork
+        .navbar-item
+          .field
             p.control
               nuxt-link.button.is-light(exact, :to="{name: 'cart'}")
                 span.icon.cartitem
                   .cartcount(v-if="total > 0") {{ total }}
                   i.fa.fa-shopping-cart
-                span Cart
+                span.is-hidden-mobile Cart
 </template>
 
 <script>
@@ -34,18 +37,21 @@ export default {
   head () {
     return {
       htmlAttrs: {
-        class: 'has-navbar-fixed-top'
+        class: this.isIndexRoute && 'has-navbar-fixed-top'
       }
     }
   },
   computed: {
-    ...mapGetters(['total'])
+    ...mapGetters(['total']),
+    isIndexRoute() {
+      return this.$route.name === 'index'
+    }
   }
 }
 </script>
 
 <style lang="stylus">
-  .hero
+  .hero-head
     .navbar
       &.is-light
         background-color #f5f5f5
@@ -54,21 +60,34 @@ export default {
         flex-wrap: wrap
         justify-content: space-between
 
-  .cartitem
-    position relative
-    float right
+    .cartitem
+      position relative
+      float right
 
-  .cartcount
-    font-family 'Barlow', sans-serif
-    position absolute
-    background #ff2211
-    color white
-    text-align center
-    padding-top 2px
-    width 18px
-    height 18px
-    font-size 10px
-    margin -8px 0 0 8px
-    border-radius 50%
-    font-weight 700
+    .cartcount
+      font-family 'Barlow', sans-serif
+      position absolute
+      background #ff2211
+      color white
+      text-align center
+      padding-top 2px
+      height 18px
+      width @height
+      font-size 10px
+      margin -8px 0 0 8px
+      border-radius 50%
+      font-weight 700
+
+    @media (max-width: 600px)
+      .button
+        padding-left 0.2rem
+        padding-right @paddingLeft
+
+        .icon
+          margin-left 0
+          margin-right 0
+
+      .navbar-item
+        padding-left .5rem
+        padding-right @paddingLeft
 </style>
